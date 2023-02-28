a7213c7a34d73cf2cfe6f1f8aef7d25bad3eb409
Hanhannif <126434813+Hanhannif@users.noreply.github.com>
2023-02-28 11:54:14 +0700

    Add files via upload

---
+++ luas lingkaran.js
@@ -0,0 +1,8 @@
+//luas lingkaran
+function rumus(b){
+}
+let b = parseInt(prompt("masukan nilai jari jari: "));
+let phi = 3.14;
+let hasil =phi * (b * b);
+console.log(hasil);
+document.write(hasil);
